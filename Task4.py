0896009d555eff97599b31dae58676d02c07584a
juliiaustymova <u.juliana.serg@gmail.com>
2022-05-23 13:33:40 +0300

    Making changes according to the comments

--- Task4.py
+++ Task4.py
@@ -1,23 +1,20 @@
-
 for i in range(10):
   text = input('Введіть текст:')
   letter = input('Введіть літеру:')
   if len(letter) > 1:
     print(f'Введено більше однієї літери.Спробуйте ще раз!')
-    letter = input('Введіть літеру:')
     continue
+
   count = 0
-  answer = 0
   for i in text :
     if i == letter:
       count = count + 1
+
   if count == 0:
       print(f'строка не містить літеру {letter}')
+  elif count < 10:
+    print(f'строка містить літеру {letter} до 10 разів ({count} разів)')
+  elif count < 20:
+      print(f'строка містить літеру {letter} до 20 раз ({count} разів)')
   else:
-    if count < 10:
-      answer = 'до 10'
-    elif count < 20:
-      answer = 'до 20'
-    else:
-      answer = 'більше 20'
-    print(f'строка містить літеру {letter} {answer} разів ({count} разів)')
+      print(f'строка містить літеру {letter} більше 20 разів ({count} разів)')
