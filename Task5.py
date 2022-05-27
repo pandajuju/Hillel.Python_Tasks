40add98fdae8dce183e16418fcde17243b62f4b3
juliiaustymova <u.juliana.serg@gmail.com>
2022-05-27 16:51:35 +0300

    Renaming variables

--- Task5.py
+++ Task5.py
@@ -1,7 +1,7 @@
 import random
 
 while True:
-  n = int(input('Введіть довжину листа:'))
+  n = int(input('Введіть довжину листа: '))
   if n < 10 or n > 50:
     print('Невірний ввод. Спробуйте ще раз!')
     continue
@@ -10,7 +10,7 @@ while True:
   for i in range(n):
     list_1.append(50 + (round(random.random() * 100) % 50))
   for i in range(n - 1):
-    for j in range(i+1, len(list_1)):
+    for j in range(i+1, n):
       if list_1[i] < list_1[j]:
         list_1[i], list_1[j] = list_1[j], list_1[i]
   print(list_1)
